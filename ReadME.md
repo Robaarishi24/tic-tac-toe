c8b4829c3d82053d4a0e867a5777c9bafa1732aa
robaarishi24 <robaarishi@hotmail.com>
2020-02-06 09:10:56 +0300

    new update

--- ReadME.md
+++ ReadME.md
@@ -1,7 +1,40 @@
-# Tic Tac Toe Game
+[![General Assembly Logo](https://camo.githubusercontent.com/1a91b05b8f4d44b5bbfb83abac2b0996d8e26c92/687474703a2f2f692e696d6775722e636f6d2f6b6538555354712e706e67)](https://generalassemb.ly/education/web-development-immersive)
+![Misk Logo](https://i.ibb.co/KmXhJbm/Webp-net-resizeimage-1.png)
 
-### Overview :
+## Tic Tac Toe Game
+![x o](https://ctl.s6img.com/society6/img/aLv5CIVe7cL0TnnKY-5BoW6ieA0/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/e20c4f0e1a054851ae53e5d97e290c3b/~~/tic-tac-toe-noughts-and-crosses-game1945373-prints.jpg?wait=0&attempt=0)
+#### Overview :
+This is my firis project it is a simple tic-tac-toe game to have some fun and can be played on browsers .
+ 
+#### Links :
+ * [My Game](https://robaarishi24.github.io/tic-tac-toe/) - You can run the game on a broswer by clicking here .
 
-A simple tic-tac-toe game to have some fun .
+ * [Wireframe](https://drive.google.com/file/d/1p4hX8uEZNa5jRhTnFhqLXLImers52AT4/view?usp=sharing) - This is the design of the page .
 
-### Technologies used :
+* [User Stories](https://app.milanote.com/1IZwCa1T0vXY1m)
+
+#### Technologies used :
+- HTML
+- CSS
+- JavaScript
+- DOM
+
+#### Game instructions  :
+- First Click on any squares you want.
+- X turn first, then O, players keep playing untill one player wins.
+- A winner can align three in a line , on any column, row, or diagonal , you can know that if all three squares
+- When a game is over you can restart it.
+
+ #### Favorite functions :
+  - The ``` .play() ``` function .
+   This function is used to play the loaded audio on the web .
+
+ #### Future plans :
+  - The players can chose X or O , name, picture . 
+  - Play with computer .
+  - Counter for win , lose or tie .
+  - print a line in win game .
+  - The game allows more than one player to play online .
+  - The players can chatting togither .
+
+ 
--- js/app.js
+++ js/app.js
@@ -1,34 +1,39 @@
-//set event click on all squares 
+//select all squares and store it in variables 
 let squares = document.querySelectorAll(".square");
-let move = 0;
+let move = 0; // count for move made it in the game 
 let turn = document.getElementById("msge"); //massege turn 
-function startGame (){
-    //to put sound for X and O
+
+
+function startGame() {
+    //to put sound when you click on the squares to put X or O
     let sound_x = new Audio('sounds/x.mp3');
     let sound_o = new Audio('sounds/o.mp3');
-    //to set X or O into the square
+   
     //can't select same square twice
     if (this.innerText === "") {
         move++;
         if (move % 2 === 0) {
+             // set X or O into the square
             sound_o.play();
             this.innerText = "O";
-             //change the massege turn 
+            //change the massege turn 
             turn.innerText = "X Turn";
             checkWinner();
-          
+
         } else {
             sound_x.play();
-            this.innerText = "X"; 
+            this.innerText = "X";
             turn.innerText = "O Turn";
             checkWinner();
         }
     }
 
 };
+
+
 //add event click
 for (let i = 0; i < squares.length; i++) {
-squares[i].addEventListener("click",startGame);
+    squares[i].addEventListener("click", startGame);
 }
 
 
@@ -47,81 +52,79 @@ function selectWinner(z1, z2, z3) {
 //check for the winner
 let zone = ['', '', '', '', '', '', '', '', ''];
 const sqr = ['', '', '', '', '', '', '', '', ''];
+
 function checkWinner() {
     for (let i = 0; i < zone.length; i++) {
-       zone[i] = document.getElementById('sqr' + i).innerText
-        sqr[i] = document.getElementById('sqr' + i);
+        zone[i] = document.getElementById('sqr' + i).innerText 
+        sqr[i] = document.getElementById('sqr' + i); 
     }
     //check the 3 row possibilities
     if (zone[0] == zone[1] && zone[1] == zone[2] && zone[0] != "") {
         turn.innerHTML = 'Player ' + zone[0] + ' Win!!';
         selectWinner(sqr[0], sqr[1], sqr[2]);
-        
-    }
-      else if (zone[3] == zone[4] && zone[4] == zone[5] && zone[3] != "") {
+
+    } else if (zone[3] == zone[4] && zone[4] == zone[5] && zone[3] != "") {
         turn.innerHTML = 'Player ' + zone[3] + ' Win!!';
         selectWinner(sqr[3], sqr[4], sqr[5]);
-    }
-      else if (zone[6] == zone[7] && zone[7] == zone[8] && zone[6] != "") {
+    } else if (zone[6] == zone[7] && zone[7] == zone[8] && zone[6] != "") {
         turn.innerHTML = 'Player ' + zone[6] + ' Win!!';
         selectWinner(sqr[6], sqr[7], sqr[8]);
     }
     //check the 3 column possibilities
-      else if (zone[0] == zone[3] && zone[3] == zone[6] && zone[0] != "") {
+    else if (zone[0] == zone[3] && zone[3] == zone[6] && zone[0] != "") {
         turn.innerHTML = 'Player ' + zone[0] + ' Win!!';
         selectWinner(sqr[0], sqr[3], sqr[6]);
-    }
-     else if (zone[1] == zone[4] && zone[4] == zone[7] && zone[1] != "") {
+    } else if (zone[1] == zone[4] && zone[4] == zone[7] && zone[1] != "") {
         turn.innerHTML = 'Player ' + zone[1] + ' Win!!';
         selectWinner(sqr[4], sqr[7], sqr[1]);
-    }
-     else if (zone[2] == zone[5] && zone[5] == zone[8] && zone[2] != "") {
+    } else if (zone[2] == zone[5] && zone[5] == zone[8] && zone[2] != "") {
         turn.innerHTML = 'Player ' + zone[2] + ' Win!!';
         selectWinner(sqr[2], sqr[5], sqr[8]);
     }
-    //check the 2 diameter possibilities
-   else if (zone[0] == zone[4] && zone[4] == zone[8] && zone[0] != "") {
+    //check the diagonally possibilities
+    else if (zone[0] == zone[4] && zone[4] == zone[8] && zone[0] != "") {
         turn.innerHTML = 'Player ' + zone[0] + ' Win!!';
         selectWinner(sqr[0], sqr[4], sqr[8]);
-    }
-     else if (zone[2] == zone[4] && zone[4] == zone[6] && zone[2] != "") {
+    } else if (zone[2] == zone[4] && zone[4] == zone[6] && zone[2] != "") {
         turn.innerHTML = 'Player ' + zone[2] + ' Win!!';
         selectWinner(sqr[2], sqr[4], sqr[6]);
-    } 
+    }
     //For Draw !!
- else if (move >= 9){
-     let drawSound = new Audio('sounds/draW.mp3');
-    turn.innerText = "Draw !!";
-    drawSound.play();
-}  
+    else if (move >= 9) {
+        let drawSound = new Audio('sounds/draw.mp3');
+        turn.innerText = "Draw !!";
+        drawSound.play();
+    }
 }
 
 
-//To stop the game 
+
+//stop the game 
 let stop = function () {
     for (let i = 0; i < squares.length; i++) {
-        squares[i].removeEventListener("click",startGame);//remove the event
-        }
+        squares[i].removeEventListener("click", startGame); //remove the event for start game
+    }
 }
+
 
 
 //To reset The the game and start Again
 let playAgain = document.querySelector('.reset');
+
 function reset() {
     //to change sound for a click
     let playSound = new Audio('sounds/play.mp3');
     playSound.play();
 
-    for (let i = 0; i < zone.length; i++) 
-    {
-   sqr[i].classList.remove("win");
-   sqr[i].innerText ='';
+    for (let i = 0; i < zone.length; i++) {
+        sqr[i].classList.remove("win"); // remove a class 
+        sqr[i].innerText = ''; // clear the board 
     }
-    turn.innerText='X Turn';
+    turn.innerText = 'X Turn';
     move = 0;
     //set event again to start a new game 
     for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("click",startGame);
+        squares[i].addEventListener("click", startGame);
     }
 }
 playAgain.addEventListener("click", reset);
